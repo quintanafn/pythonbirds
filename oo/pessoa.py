4743a2328a459033d4b09c24ddd9b3d29667c3f1
Quintanafn <jvquintana34@gmail.com>
2021-04-18 23:02:28 -0400

    Criada o método complementar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'olá { id (self) }'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('vitor')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-
+    p.nome = ' joao'
+    print(p.nome)
 
